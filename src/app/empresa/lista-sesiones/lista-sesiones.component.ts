5f983bf0a4e497a313da6e2ab74584e7b369f438
Gonzalo Alonso <zalo29@gmail.com>
2020-10-27 12:32:00 +0200

    lista-sesiones implemented

--- src/app/empresa/lista-sesiones/lista-sesiones.component.ts
+++ src/app/empresa/lista-sesiones/lista-sesiones.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit, OnChanges } from '@angular/core';
-import { Session } from 'protractor';
+import { Empresa } from 'src/app/models/empresa';
+import { Sesion } from 'src/app/models/sesion';
+import { Solario } from 'src/app/models/solarios';
 import { Usuario } from 'src/app/models/usuario';
 import { EmpresasService } from 'src/app/services/empresas.service';
 
@@ -12,12 +14,25 @@ export class ListaSesionesComponent implements OnInit {
 
   @Input() user: Usuario;
 
-  public listaSesiones: Session[];
+  public listaSesiones: Sesion[];
+  public empresa: Empresa;
 
-  constructor(private empSrv: EmpresasService) { }
+  constructor(private empSrv: EmpresasService) {
+    this.empSrv.getDataEmpresa().subscribe(
+      resp => {
+        this.empresa = resp.datos;
+        //console.log("DATOS DE EMPRESA: " + JSON.stringify(this.empresa))
+      }
+    );
+  }
 
   ngOnInit() {
     this.loadSesiones();
+    this.empSrv.notification.subscribe(
+      evt => {
+        this.loadSesiones();
+      }
+    );
   }
 
   ngOnChanges() {
